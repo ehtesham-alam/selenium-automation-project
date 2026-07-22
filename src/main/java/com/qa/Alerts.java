0d74513034faae516cac1d5624f9fd1f4b2224dc
Ehtesham Alam <ehtesham.xyz7@gmail.com>
2026-07-23 05:04:06 +0530

    Added API automation

--- src/main/java/com/qa/Alerts.java
+++ src/main/java/com/qa/Alerts.java
@@ -37,6 +37,8 @@ public class Alerts {
 		 
 			// find alert button and perform click action
 			// find alert button and perform click action
+			// find alert button and perform click action
+
 
 
 
